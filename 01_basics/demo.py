b1f9d38861bd301bc263e9f2352e072a27c1ace8
chittampallimanoj3 <chittampallimanoj3@gmail.com>
2025-09-17 10:15:39 +0530

    practice

---
+++ 01_basics/demo.py
@@ -0,0 +1,19 @@
+# means comment, python will skip execution of this line
+#we used script mode to execute below code
+a=10
+b=20
+print(a+b)
+
+
+print("good morning")
+print('good morning')
+
+# print(hello) #NameError: name 'hello' is not defined. Did you mean: 'help'?
+
+hello="hello hi"
+print(hello)
+
+a=200
+b=18
+percent=(a/100*b)
+print(percent)
---
+++ 01_basics/variable.py
@@ -0,0 +1,37 @@
+#variable=value
+# a=10
+# print(a)
+
+# a,b,c=1
+# print(a,b,c) #bcz it is not possible to give 3 variables for 1 value
+ 
+# a,b,c=1,3,4
+# print(a,b,c)
+
+# c=1,2,3
+# print(c) #here we can give no.of values to 1 varibale
+
+# c=v=x=22
+# print(c)
+# # print(c,v,x)
+
+# abc=3
+# print(abc)
+
+# 23abc=5
+# print(23abc)
+
+# _abc=2
+# print(_abc)
+
+# &abc231=5
+# peint(&abc231)
+
+# abc123=8
+# print(id(abc123))    #to find memory loaction of the variable  we use method id(
+
+# pythonlife=1
+# print(pythonlife)
+
+(PYTHONLIFE)=11
+print(PYTHONLIFE)
