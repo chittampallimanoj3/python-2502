83f6626f9fa657e362b8e6483e521af046530df4
chittampallimanoj3 <chittampallimanoj3@gmail.com>
2025-09-18 18:21:15 +0530

    Arithmetic operators practice

---
+++ 01_basics/task_01/operators/demo.py
@@ -0,0 +1,44 @@
+#variables for demonstration
+a=10
+b=5
+
+#1.Addition(+)
+#Adds two operands.
+sum_result=a+b
+print(f"Addition ({a} + {b}): {sum_result}")
+
+
+#2.subtraction(-)
+#subtracts the right operand from the left operand.
+difference_result=a-b
+print(f"subtraction ({a} -{b}): {difference_result}")
+
+
+#3.Multiplication(*)
+#Multiplies two operands.
+# product_result =a*b
+# print(f"Multiplication({a} * {b}): {product_result}")
+
+
+#4. 4Division (/)
+#Divides the left operand by the right operand,always returning a float,
+division_result = a / b
+print(f"Division ({a} / {b}): {division_result}")
+
+
+#5. Floor Division (//)
+# Divides the left operand by the right operand and returns the integer part of the quotient (rounds down).
+floor_division_result = a // b
+print(f"FloorDivision ({a} // {b}): {floor_division_result}") 
+
+
+#6.Modulus (%)
+#Returns the remainder of the division.
+modulus_result = a % b
+print(f"Modulus ( {a} % {b}): {modulus_result}")
+
+
+#7.Exponentiation (**)
+#Raises the left operand to the power of the right operand.
+exponentiation_result = a**b
+print(f"Exponentiation ( {a} ** {b}): {exponentiation_result}")
