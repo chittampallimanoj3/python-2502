f8639bd1327264ea7e97a815bb0e117478244361
chittampallimanoj3 <chittampallimanoj3@gmail.com>
2025-09-19 10:35:41 +0530

    data types

---
+++ 01_basics/task_01/operators/data types/practice.py
@@ -0,0 +1,35 @@
+#Data types
+""
+# integer
+# boolean
+# float
+# complex
+
+# #integer
+# a=10
+# b=-22
+# print(type(a),type(b))
+
+# #boolean
+# a=True
+# b=False
+# print(type(a),type(b))
+
+# print(True==1)
+# print(False==0)
+
+# #Float
+# c=4.44
+# print(type(c))
+
+# #Complex
+# v=2+5j
+# print(type(v))
+
+# #coverting int to float
+# a=2
+# print(float(a))
+
+#coverting float to int
+a=2.2
+print(int(a))
