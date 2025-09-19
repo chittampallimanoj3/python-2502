0d5ef5579192d62a1b34a4c6227d0c3ea4227ccf
chittampallimanoj3 <chittampallimanoj3@gmail.com>
2025-09-19 18:32:43 +0530

    conditional stmnts

---
+++ 01_basics/task_01/operators/data types/conditional.py
@@ -0,0 +1,35 @@
+# #Conditional statements or Decision making statements
+
+# #syntax:-
+# if condition: if stmt should execute in if condition the condition should be True.
+# -->if the condition is false then it automatically goes to else condition
+
+# if True:
+#     print("hi manoj")
+# else:
+#     print("bye")
+
+
+# if False:
+#     print("hi manoj")
+# else:
+#     print("bye")
+
+
+
+# age=18
+# if age>18:
+#     print("you can vote")
+# elif age==18:
+#     print("you can vote buddy")
+# else:
+#     print("you are under age") 
+
+
+age=18
+if age>18:
+    print("you can vote")
+elif age<18:
+    print("you can vote buddy")
+else:
+    print("you are under age") 
