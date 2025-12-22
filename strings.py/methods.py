21612df9045a250c44217304827ec970d6fedc5c
chittampallimanoj3 <chittampallimanoj3@gmail.com>
2025-12-22 12:23:42 +0530

    strings

---
+++ strings.py/methods.py
@@ -0,0 +1,42 @@
+#methods
+# # pythonlife = "please subscribe"
+# print(pythonlife.upper())
+# print(pythonlife.lower())
+
+
+#ends with
+
+# pythonlife = "python language"
+# print(pythonlife.endswith("language"))
+# print(pythonlife.endswith("python"))
+
+#Replace
+
+# pythonlife = "python language"
+# print(pythonlife.replace("language","programming"))
+
+# Index& find
+# pythonlife = "python language"
+# print(pythonlife.index("python"))
+# print(pythonlife.index("language"))
+# print(pythonlife.find("python"))
+# print(pythonlife.find("language"))
+
+
+#count
+
+# pythonlife = "python language"
+# print(pythonlife.count("p"))
+# print(pythonlife.count("g"))
+
+# split
+
+pythonlife="python language"
+print(pythonlife.split())
+
+
+#strip
+
+pythonlife = "         python language      "
+print(pythonlife)
+print(len(pythonlife))
